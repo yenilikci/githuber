0819ff8ee2887a9a1db612f3250dd4086438463a
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-09 20:12:08 +0300

    github api getting token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 function Home(props) {
+    const a = process.env.REACT_APP_GITHUB_URL
+    console.log(a)
     return (
         <div>
             <h1 className="text-6xl">
