b517f69d6413153c13c2eba2233f490e4d7abaf7
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-09 23:06:39 +0300

    add UserResults component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import UserResults from "../components/users/UserResults";
 
 function Home(props) {
-    const a = process.env.REACT_APP_GITHUB_URL
-    console.log(a)
     return (
-        <div>
-            <h1 className="text-6xl">
-                Welcome
-            </h1>
-        </div>
+        <>
+            {/* Search Component */}
+            <UserResults/>
+        </>
     );
 }
 
