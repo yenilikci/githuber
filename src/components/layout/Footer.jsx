4e60161e2adffa9c6331784426714e74a4114c57
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-09 19:33:38 +0300

    add footer component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer(props) {
+    const footerYear = new Date().getFullYear()
+    return (
+        <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
+            <div>
+                <p>
+                    Copyright &copy; {footerYear} All rights reserved.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
