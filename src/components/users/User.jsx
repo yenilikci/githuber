33de28c082871f231005e239990af43c673f2331
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-13 01:47:25 +0300

    profile stats

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -2,6 +2,7 @@ import {useContext, useEffect} from 'react';
 import ReactLoading from 'react-loading';
 import GithubContext from '../../context/github/GithubContext';
 import {Link} from "react-router-dom";
+import {FaCodepen, FaStore, FaUserFriends, FaUsers} from "react-icons/fa";
 
 function User({match}) {
     const {getUser, user, loading} = useContext(GithubContext);
@@ -76,9 +77,91 @@ function User({match}) {
                                 </a>
                             </div>
                         </div>
+
+                        <div className="w-full rounded-lg shadow-md bg-base-100 stats">
+                            {location && (
+                                <div className="stat">
+                                    <div className="stat-title text-md">Location</div>
+                                    <div className="text-lg stat-value">{location}</div>
+                                </div>
+                            )}
+
+                            {blog && (
+                                <div className="stat">
+                                    <div className="stat-title text-md">Website</div>
+                                    <div className="text-lg stat-value">
+                                        <a href={`https://${blog}`} target="_blank" rel="noreferrer">
+                                            {blog}
+                                        </a>
+                                    </div>
+                                </div>
+                            )}
+
+                            {twitter_username && (
+                                <div className="stat">
+                                    <div className="stat-title text-md">Website</div>
+                                    <div className="text-lg stat-value">
+                                        <a href={`https://twitter.com/${twitter_username}`} target="_blank"
+                                           rel="noreferrer">
+                                            {twitter_username}
+                                        </a>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
 
+                <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+                    <div className="stat">
+                        <div className="stat-figure text-primary">
+                            <FaUsers className="text-3xl md:text-5xl"/>
+                        </div>
+                        <div className="stat-title pr-5">
+                            Followers
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:text-4xl">
+                            {followers}
+                        </div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-primary">
+                            <FaUserFriends className="text-3xl md:text-5xl"/>
+                        </div>
+                        <div className="stat-title pr-5">
+                            Following
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:text-4xl">
+                            {following}
+                        </div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-primary">
+                            <FaCodepen className="text-3xl md:text-5xl"/>
+                        </div>
+                        <div className="stat-title pr-5">
+                            Public Repos
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:text-4xl">
+                            {public_repos}
+                        </div>
+                    </div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-primary">
+                            <FaStore className="text-3xl md:text-5xl"/>
+                        </div>
+                        <div className="stat-title pr-5">
+                            Public Gists
+                        </div>
+                        <div className="stat-value pr-5 text-3xl md:text-4xl">
+                            {public_gists}
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </>
     );
