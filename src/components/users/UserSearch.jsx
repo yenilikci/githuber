0afc56e2653f12f65697ecab59e9ce2b187f955c
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-02-13 03:50:32 +0300

    move searchUsers to actions file

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,21 +1,24 @@
 import React, {useContext, useState} from 'react';
 import GithubContext from "../../context/github/GithubContext";
 import AlertContext from "../../context/alert/AlertContext";
+import {searchUsers} from "../../context/github/GithubActions";
 
 const UserSearch = () => {
     const [text, setText] = useState('');
 
-    const {users, searchUsers, clearUsers} = useContext(GithubContext);
+    const {users, clearUsers, dispatch} = useContext(GithubContext);
 
     const {setAlert} = useContext(AlertContext);
 
     const handleChange = (e) => setText(e.target.value);
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (text === '') {
             setAlert('Please enter something in the search box', 'Error');
         } else {
-            searchUsers(text);
+            dispatch({type: 'SET_LOADING'});
+            const users = await searchUsers(text);
+            dispatch({type: 'GET_USERS', payload: users});
             setText('');
         }
     };
--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -17,28 +17,6 @@ export const GithubProvider = ({children}) => {
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
 
-    //get search results
-    const searchUsers = async (text) => {
-        setLoading();
-
-        const params = new URLSearchParams({
-            q: text
-        });
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        });
-
-        const {items} = await response.json();
-
-        dispatch({
-            type: 'GET_USERS',
-            payload: items
-        })
-    }
-
     //get single user
     const getUser = async (login) => {
         setLoading();
@@ -92,11 +70,8 @@ export const GithubProvider = ({children}) => {
 
     return <GithubContext.Provider
         value={{
-            users: state.users,
-            loading: state.loading,
-            user: state.user,
-            repos: state.repos,
-            searchUsers,
+            ...state,
+            dispatch,
             clearUsers,
             getUser,
             getUserRepos
